f9b982b77f19030b26c33845d8a4ef56937acf50
Artur Azarau <artur.azarau@gmail.com>
2019-12-02 23:03:44 +0300

    Task 3

---
+++ markers.cpp
@@ -0,0 +1,38 @@
+#include "ros/ros.h" 
+#include "std_msgs/Int32.h" 
+#include "geomerty_msgs/Point.h"
+#include "visualization_msgs/Marker.h"
+
+class SubscribeAndPublish {
+public:
+	SubscribeAndPublish() {
+		publisher = n.advertise<visualization_msgs::Marker>("/output", 10);
+		subscriber = n.subscribe("/input", 10, &SubscribeAndPublish::callback, this);
+	}
+
+	void callback(const geomerty_msgs::Point& point) {
+		visualization_msgs::Marker marker;
+		marker.header.frame_id = "points";
+		marker.ns = "namespace";
+		marker.id = counter;
+		marker.type = visualization_msgs::Marker::POINTS;
+		marker.action = visualization_msgs::Marker::ADD;
+		marker.point.push_back(point);
+		publisher.publish(marker);
+		counter++;
+	}
+
+private:
+	std_msgs::Int32 type = 1;
+	std_msgs:: Int32 counter = 0;
+	ros::NodeHandle n;
+	ros::Publisher publisher;
+	ros::Subscriber subscriber;
+};
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "subscribe_and_publish");
+	SubscribeAndPublish SAPObject;
+	ros::spin();
+	return 0;
+}
