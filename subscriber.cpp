7ee2eeaa50144f2449cf55df4adf91ba87c486cc
Artur Azarau <artur.azarau@gmail.com>
2019-12-02 22:32:59 +0300

    second task

---
+++ subscriber.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h" 
+#include <iostream> 
+#include "std_msgs/Int32.h" 
+
+using namespace std; 
+
+void receive(const std_msgs::Int32& msh) {
+	cout << ros::this_node::getName() << ": data: " << msg.data << endl;
+	return;
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "subscriber");
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("/topic", 1000, receive);
+	ros::spin();
+	return 0;
+}
